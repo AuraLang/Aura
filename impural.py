d3f196c660bba9fba9c6c47947e7fbdf8969944f
iWaroz <iwarozmc@gmail.com>
2022-06-04 11:36:32 +0200

    Moved token output logic to single function + .gitignore

--- impural.py
+++ impural.py
@@ -1,14 +1,16 @@
 import sys
 
-import interpreter
+import interpreter.lexer.tokenizer
+import interpreter.tokens
 
 def main():
-    source_name = sys.argv[1]
+    source_name = "impural/samples/fizzbuzz.im" #sys.argv[1]
 
     with open(source_name) as source_file:
         source_code = source_file.read()
     
     tokens: list = interpreter.lexer.tokenizer.tokenize(source_code)
+    interpreter.tokens.output_token_list(tokens)
 
 if __name__ == "__main__":
     main()
--- interpreter/tokens.py
+++ interpreter/tokens.py
@@ -1,30 +1,30 @@
+def output_token_list(tokens: list):
+    out: list = []
+    for token in tokens:
+        if token.details != None:
+            out.append(f"{token.__class__.__name__}<\033[92m{token.details}\033[0m>")
+        else:
+            out.append(token.__class__.__name__)
+    print(" ".join(out))
+
 class RawToken:
     def __init__(self, details=None):
         self.details = details
-    
-    def __repr__(self):
-        if self.details == None:
-            return f"{self.__class__.__name__}"
-        return f"{self.__class__.__name__}<\033[92m{self.detail_repr()}\033[0m>"
 
 class Name(RawToken):
-    def detail_repr(self):
-        return self.details
+    ...
 
 class Literal(RawToken):
     ...
 
 class LiteralStr(Literal):
-    def detail_repr(self):
-        return f'{self.details}'
+    ...
 
 class LiteralNum(Literal):
-    def detail_repr(self):
-        return self.details
+    ...
 
 class Operator(RawToken):
-    def detail_repr(self):
-        return self.details
+    ...
 
 class NewLine(RawToken):
     ...
